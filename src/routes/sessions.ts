bc9e3560f4dc1a12c751ee717935b3585ff4d3e2
otobot1 <otobot1@gmail.com>
2022-05-23 14:45:06 -0600

    formatting

--- src/routes/sessions.ts
+++ src/routes/sessions.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { prisma } from "../prismaClient";
+
 import { isErrorWithMessage, toErrorWithMessage, noRouteError, errorHandler, methodNotAllowed } from "../errorHandling";
 import {
     formatSession, noUserWithDiscordIdErrorMessage, regenerateSessionAsync, revokeSessionAsync, storeIdentityInSession, adminPermsArray,
