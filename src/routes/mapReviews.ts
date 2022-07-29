9e700536cd071e1ef7701fa3276bc88812396e9b
otobot1 <otobot1@gmail.com>
2022-07-29 16:36:45 -0600

    create all routes (with empty functions)

--- src/routes/mapReviews.ts
+++ src/routes/mapReviews.ts
@@ -8,7 +8,178 @@ import { isErrorWithMessage, toErrorWithMessage, noRouteError, errorHandler, met
 import { reviews_maps } from ".prisma/client";
 // import { formattedUser, permissions } from "../types/frontend";
 // import { createUserData, updateUserData } from "../types/internal";
+import { expressRoute } from "../types/express";
 
 
 const router = express.Router();
 export { router as mapReviewsRouter };
+
+
+
+
+router.route("/")
+    .get(async function (req, res, next) {
+        try {
+
+        }
+        catch (error) {
+            next(error);
+        }
+    })
+    .all(methodNotAllowed);
+
+
+
+
+router.route("/search")
+    .all(noRouteError);
+
+
+router.route("/search/map")
+    .get(async function (req, res, next) {
+        try {
+
+        }
+        catch (error) {
+            next(error);
+        }
+    })
+    .all(methodNotAllowed);
+
+
+router.route("/search/user")
+    .get(async function (req, res, next) {
+        try {
+
+        }
+        catch (error) {
+            next(error);
+        }
+    })
+    .all(methodNotAllowed);
+
+
+
+
+router.param("modID", async function (req, res, next) {
+    try {
+
+    }
+    catch (error) {
+        next(error);
+    }
+});
+
+
+router.route("/mod/:modID")
+    .get(async function (req, res, next) {
+        try {
+
+        }
+        catch (error) {
+            next(error);
+        }
+    })
+    .all(methodNotAllowed);
+
+
+
+
+router.param("mapID", async function (req, res, next) {
+    try {
+
+    }
+    catch (error) {
+        next(error);
+    }
+});
+
+
+router.route("/map/:mapID")
+    .get(async function (req, res, next) {
+        try {
+
+        }
+        catch (error) {
+            next(error);
+        }
+    })
+    .all(methodNotAllowed);
+
+
+
+
+router.param("userID", async function (req, res, next) {
+    try {
+
+    }
+    catch (error) {
+        next(error);
+    }
+});
+
+
+router.route("/user/:userID")
+    .get(async function (req, res, next) {
+        try {
+
+        }
+        catch (error) {
+            next(error);
+        }
+    })
+    .all(methodNotAllowed);
+
+
+
+
+router.param("mapReviewID", async function (req, res, next) {
+    try {
+
+    }
+    catch (error) {
+        next(error);
+    }
+});
+
+
+router.route("/:mapReviewID")
+    .get(async function (req, res, next) {
+        try {
+
+        }
+        catch (error) {
+            next(error);
+        }
+    })
+    .patch(async function (req, res, next) {
+        try {
+
+        }
+        catch (error) {
+            next(error);
+        }
+    })
+    .delete(async function (req, res, next) {
+        try {
+
+        }
+        catch (error) {
+            next(error);
+        }
+    })
+    .all(methodNotAllowed);
+
+
+
+
+router.use(noRouteError);
+
+router.use(errorHandler);
+
+
+
+
+export const mapReviewPost = <expressRoute>async function (req, res, next) {    //called from ./reviews.ts
+
+}
--- src/routes/reviews.ts
+++ src/routes/reviews.ts
@@ -12,3 +12,150 @@ import { reviews } from ".prisma/client";
 
 const router = express.Router();
 export { router as reviewsRouter };
+
+
+
+
+router.route("/")
+    .get(async function (req, res, next) {
+        try {
+
+        }
+        catch (error) {
+            next(error);
+        }
+    })
+    .post(async function (req, res, next) {
+        try {
+
+        }
+        catch (error) {
+            next(error);
+        }
+    })
+    .all(methodNotAllowed);
+
+
+
+
+router.route("/search")
+    .all(noRouteError);
+
+
+router.route("/search/mod")
+    .get(async function (req, res, next) {
+        try {
+
+        }
+        catch (error) {
+            next(error);
+        }
+    })
+    .all(methodNotAllowed);
+
+
+router.route("/search/user")
+    .get(async function (req, res, next) {
+        try {
+
+        }
+        catch (error) {
+            next(error);
+        }
+    })
+    .all(methodNotAllowed);
+
+
+
+
+router.param("modID", async function (req, res, next) {
+    try {
+
+    }
+    catch (error) {
+        next(error);
+    }
+});
+
+
+router.route("/mods/:modID")
+    .get(async function (req, res, next) {
+        try {
+
+        }
+        catch (error) {
+            next(error);
+        }
+    })
+    .all(methodNotAllowed);
+
+
+
+
+router.param("userID", async function (req, res, next) {
+    try {
+
+    }
+    catch (error) {
+        next(error);
+    }
+});
+
+
+router.route("/users/:userID")
+    .get(async function (req, res, next) {
+        try {
+
+        }
+        catch (error) {
+            next(error);
+        }
+    })
+    .all(methodNotAllowed);
+
+
+
+
+router.param("reviewID", async function (req, res, next) {
+    try {
+
+    }
+    catch (error) {
+        next(error);
+    }
+});
+
+
+router.route("/:reviewID")
+    .get(async function (req, res, next) {
+        try {
+
+        }
+        catch (error) {
+            next(error);
+        }
+    })
+    .patch(async function (req, res, next) {
+        try {
+
+        }
+        catch (error) {
+            next(error);
+        }
+    })
+    .delete(async function (req, res, next) {
+        try {
+
+        }
+        catch (error) {
+            next(error);
+        }
+    })
+    .all(methodNotAllowed);
+
+
+
+
+router.use(noRouteError);
+
+router.use(errorHandler);
