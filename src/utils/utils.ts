7d53ae348192a0d214586b73348572d7c7289bd5
otobot1 <otobot1@gmail.com>
2022-08-28 21:30:13 -0600

    add getAverage util function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,14 @@
-export const getCurrentTime = function () {
+export const getCurrentTime = () => {
     return Math.floor(new Date().getTime() / 1000);
+}
+
+
+
+
+export const getAverage = (numbers: number[]) => {
+    if (!numbers.length) return;
+
+    const sum = numbers.reduce((sum, number) => sum + number, 0);
+
+    return sum / numbers.length;
 }
