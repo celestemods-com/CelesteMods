d81cc5f61c8a8aa43216938204f6f26aaf029915
otobot1 <otobot1@gmail.com>
2022-09-03 18:47:06 -0600

    comment out unused properties

--- src/constants/integerSizes.ts
+++ src/constants/integerSizes.ts
@@ -15,10 +15,10 @@ export const intMaxSizes = {
         signed: 2147483647 - 1,
         unsigned: 4294967295 - 1,
     },
-    bigInt: {
-        signed: 9223372036854775807n - 1n,
-        unsigned: 18446744073709551615n - 1n,
-    },
+    // bigInt: {
+    //     signed: 9223372036854775807n - 1n,
+    //     unsigned: 18446744073709551615n - 1n,
+    // },
 }
 
 //the -1 on each line is intended as an extra safety measure against trimmed out-of-range numbers ever matching a valid number
