a3f81f66de0629e2206cb7c52709ca90bac6da95
otobot1 <otobot1@gmail.com>
2022-10-02 01:54:41 -0600

    fix .env not being loaded

--- express-backend/src/index.ts
+++ express-backend/src/index.ts
@@ -1,3 +1,6 @@
+import * as dotenv from "dotenv";
+dotenv.config({path: "../.env"});
+
 import express from "express";
 import cookieParser from "cookie-parser";
 import { sessionCookieNameString, sessionMiddleware } from "./middlewaresAndConfigs/sessionMiddleware";
