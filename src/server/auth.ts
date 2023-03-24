508c7e7e3086578389d6b4b07afd484eb744e4be
otobot1 <otobot1@gmail.com>
2023-03-24 13:50:36 -0600

    rename db.ts to prisma.ts and add comment

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -7,7 +7,7 @@ import {
 import DiscordProvider from "next-auth/providers/discord";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "~/env.mjs";
-import { prisma } from "~/server/db";
+import { prisma } from "~/server/prisma";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
