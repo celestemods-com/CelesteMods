2e378ec199cad17eb5c82e3d5d057aa66ed871bd
otobot1 <otobot1@gmail.com>
2023-05-07 16:49:38 -0600

    install mantine
    initial config for mantine
    start working on Layout/AppShell

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,40 @@
+import "~/styles/globals.css";
+
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { MantineProvider } from "@mantine/core";
 
 import { api } from "~/utils/api";
+import { RouterTransition } from "~/components/routerTransition";
+import { emotionCache } from "~/emotionCache";
 
-import "~/styles/globals.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
+
+
+const MyApp: AppType<{ session: Session | null; }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <MantineProvider
+        emotionCache={emotionCache}
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: "dark",
+        }}
+      >
+        <RouterTransition />
+        <Component {...pageProps} />
+      </MantineProvider>
     </SessionProvider>
   );
 };
+
+
+
 
 export default api.withTRPC(MyApp);
---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { DocumentContext } from 'next/document';
+import { ServerStyles, createStylesServer } from '@mantine/next';
+import { emotionCache } from "~/emotionCache";
+
+
+
+
+const stylesServer = createStylesServer(emotionCache);
+
+
+export default class _Document extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      styles: [
+        initialProps.styles,
+        <ServerStyles html={initialProps.html} server={stylesServer} key="styles" />,
+      ],
+    };
+  }
+}
