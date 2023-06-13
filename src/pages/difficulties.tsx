242774229b06a2cb0d55d0ab78d58c09374e1ab2
otobot1 <otobot1@gmail.com>
2023-06-12 22:16:56 -0600

    Create difficulties.tsx

---
+++ src/pages/difficulties.tsx
@@ -0,0 +1,12 @@
+import { type NextPage } from "next";
+import { DataTable } from "mantine-datatable";
+import { api } from "~/utils/api";
+
+
+
+
+const Difficulties: NextPage = () => {
+    
+};
+
+export default Difficulties;
