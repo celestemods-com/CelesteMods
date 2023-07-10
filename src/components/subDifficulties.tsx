4de2aab09917ec7dbe66a21bcae45703a412cd17
otobot1 <otobot1@gmail.com>
2023-07-10 01:26:37 -0600

    continue building Difficulty page

--- src/components/subDifficulties.tsx
+++ src/components/subDifficulties.tsx
@@ -1,27 +1,52 @@
+import { createStyles } from "@mantine/core";
 import { DataTable } from "mantine-datatable";
 import { Difficulty } from "~/pages/difficulties";
-import { RouterOutputs } from "~/utils/api";
+
+
+
+
+const useStyles = createStyles(
+    (theme) => ({
+        subDifficulty: {
+            // double ampersand to increase selectivity of class to ensure it overrides any other css
+            "&&": {
+                /* top | left and right | bottom */
+                margin: `${theme.spacing.xs} ${theme.spacing.sm} ${theme.spacing.xl}`
+            },
+        },
+    }),
+);
 
 
 
 
 type SubDifficultiesProps = {
     isLoading: boolean;
+    isExpanded: boolean;
     subDifficulties: Difficulty[];
 };
 
 
 
-export default function SubDifficulties({ isLoading, subDifficulties }: SubDifficultiesProps) {
+export default function SubDifficulties({ isLoading, isExpanded, subDifficulties }: SubDifficultiesProps) {
+    const { cx, classes } = useStyles();
     return (
         <DataTable
+            className={cx(isExpanded && classes.subDifficulty)}
             withBorder
             borderRadius={"lg"}
             striped
             withColumnBorders
             highlightOnHover
             fetching={isLoading}
-            records={subDifficulties}
+            records={[
+                ...subDifficulties,
+                // {
+                //     name: "extraDifficulty",
+                //     description: "description",
+                //     order: 99,
+                // },
+            ]}
             columns={[
                 { accessor: "name", title: "Name", sortable: true },
                 { accessor: "description", title: "Description", sortable: true },
--- src/pages/difficulties.tsx
+++ src/pages/difficulties.tsx
@@ -4,6 +4,27 @@ import { RouterOutputs, api } from "~/utils/api";
 import PageHeader from "~/components/pageHeader";
 import { useMemo, useState } from "react";
 import SubDifficulties from "~/components/subDifficulties";
+import { createStyles } from "@mantine/core";
+
+
+
+
+const useStyles = createStyles(
+    (theme) => ({
+        difficultyCell: {
+            //double ampersand to increase selectivity of class to ensure it overrides any other css
+            "&&": {
+                /* top | left and right | bottom */
+                padding: `${theme.spacing.xl} ${theme.spacing.xl} ${theme.spacing.xl}`,
+            },
+        },
+        expandedDifficultyCell: {
+            "&&": {
+                paddingBottom: 0,
+            }
+        }
+    }),
+);
 
 
 
@@ -11,64 +32,101 @@ import SubDifficulties from "~/components/subDifficulties";
 export type Difficulty = RouterOutputs["difficulty"]["getById"];
 
 
+type SortedDifficulty = {
+    parent: Difficulty;
+    children: Difficulty[];
+};
+
+
 
 
 const Difficulties: NextPage = () => {
-    const difficultyQuery = api.difficulty.getAll.useQuery({});
+    const difficultyQuery = api.difficulty.getAll.useQuery({}, { queryKey: ["difficulty.getAll", {}] });
     const difficulties = difficultyQuery.data ?? [];
 
 
-    const parentDifficulties = difficulties.filter(
-        ({ parentDifficultyId }) => parentDifficultyId === 0,
-    ).sort();
+    const sortedDifficulties = useMemo(() => {
+        const parentDifficulties = difficulties.filter(
+            ({ parentDifficultyId }) => parentDifficultyId === 0,
+        ).sort();
 
 
-    const sortedDifficulties = parentDifficulties.map(
-        (parentDifficulty) => {
-            if (parentDifficulty.parentDifficultyId !== 0) throw "Parent difficulty parentDifficultyId must be 0";
+        return parentDifficulties.map(
+            (parentDifficulty) => {
+                if (parentDifficulty.parentDifficultyId !== 0) throw "Parent difficulty parentDifficultyId must be 0";
 
 
-            const childDifficultiesArray = difficulties.filter(
-                ({ parentDifficultyId }) => parentDifficultyId === parentDifficulty.id,
-            ).sort();
+                const childDifficultiesArray = difficulties.filter(
+                    ({ parentDifficultyId }) => parentDifficultyId === parentDifficulty.id,
+                ).sort();
 
 
-            return {
-                parent: parentDifficulty,
-                children: childDifficultiesArray,
-            };
-        }
-    );
+                return {
+                    parent: parentDifficulty,
+                    children: childDifficultiesArray,
+                };
+            }
+        );
+    }, [difficulties]);
 
+
+    const [expandedRowIds, setExpandedRowsIds] = useState<number[]>(sortedDifficulties.map((expandedRow) => expandedRow.parent.id));
+
+
+
+    const { cx, classes } = useStyles();
 
     return (
         <>
             <PageHeader title="Difficulties" />
             <DataTable
+                defaultColumnProps={{
+                    cellsClassName: (record) => {
+                        return cx(
+                            classes.difficultyCell,
+                            record.isExpanded && classes.expandedDifficultyCell,
+                        );
+                    },
+                }}
                 withBorder
                 borderRadius={"sm"}
                 striped
                 withColumnBorders
                 highlightOnHover
                 fetching={difficultyQuery.isLoading}
-                records={parentDifficulties}
+                records={sortedDifficulties.map(
+                    (sortedDifficulty) => {
+                        return ({
+                            ...sortedDifficulty.parent,
+                            isExpanded: expandedRowIds.some(
+                                (id) => id === sortedDifficulty.parent.id,
+                            ),
+                        });
+                    },
+                )}
+                idAccessor={(record) => record.id}
                 columns={[
                     { accessor: "name", title: "Name", sortable: true },
                     { accessor: "description", title: "Description", sortable: true },
                     { accessor: "order", title: "Order", sortable: true },
                 ]}
                 rowExpansion={{
-                    trigger: "always",
+                    trigger: "click",
                     allowMultiple: true,
+                    expanded: {
+                        recordIds: expandedRowIds,
+                        onRecordIdsChange: setExpandedRowsIds,
+                    },
                     content: ({ record }) => {
                         const childDifficulties = sortedDifficulties.find(({ parent }) => parent.id === record.id)?.children;
 
                         if (!childDifficulties) return null;
 
-                        
+
                         return (
                             <SubDifficulties
                                 isLoading={difficultyQuery.isLoading}
+                                isExpanded={record.isExpanded}
                                 subDifficulties={childDifficulties}
                             />
                         );
