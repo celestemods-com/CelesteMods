3c4e45f436d334154a2618b69374680565f862dd
otobot1 <otobot1@gmail.com>
2023-07-20 14:53:58 -0600

    add comments

--- src/server/auth/discordProviderConfig.ts
+++ src/server/auth/discordProviderConfig.ts
@@ -6,10 +6,13 @@ import { commonProfileCallbackParams } from "./auth";
 
 
 
-
+//TODO!: add functionality to intercept typical auth flow for "unlinked" users and redirect them to a page where they can link their account. probably in ./auth.ts
 const profileCallback = (profile: DiscordProfile): Awaitable<User> => {
     if (profile.avatar === null) {
-        const defaultAvatarNumber = parseInt(profile.discriminator) % 5;
+        const defaultAvatarNumber = (
+            parseInt(profile.discriminator) ||
+            Math.random() * 10000   //if the discriminator is not a number (or is 0), use a random number
+        ) % 5;
         profile.image_url = `https://cdn.discordapp.com/embed/avatars/${defaultAvatarNumber}.png`;
     } else {
         const format = profile.avatar.startsWith("a_") ? "gif" : "png";
