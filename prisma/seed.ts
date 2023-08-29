ee96485d9d025c66754befc99e53f8393c63a9af
otobot1 <otobot1@gmail.com>
2023-08-29 10:56:27 -0600

    Update seed.ts
    Change some comments to JSDoc style so they show up in IntelliSense.
    
    Increase white space.
    
    Change hard-coded string variable to all-caps.
    
    Add omitted semicolons.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,41 +3,65 @@ import { ModType, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-// Random integer from min (included) to max (excluded).
+
+
+
+/** Random integer from min (included) to max (excluded). */
 function randomInteger(min: number, max: number) {
     return min + Math.floor(Math.random() * (max - min));
 }
 
+
 function randomString(size: number) {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    let res = '';
-    for (let _ = 0; _ < size; _++) {
-        res += characters[randomInteger(0, characters.length)];
+    const CHARACTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+
+    let result = '';
+
+    for (let i = 0; i < size; i++) {
+        result += CHARACTERS[randomInteger(0, CHARACTERS.length)];
     }
-    return res;
+
+
+    return result;
 }
+
 
 function randomElement<T>(array: T[]) {
     if (array.length === 0) {
         return null;
     }
-    return array[randomInteger(0, array.length)]!;
+
+
+    return array[randomInteger(0, array.length)];
 }
 
-// List of distinct integers from min (included) to max (excluded).
-// The function keeps randomly choosing numbers until it gets a distinct number,
-// so only use this function when n is relatively smaller than max - min.
+
+/**
+ * List of distinct integers from min (included) to max (excluded).
+ * The function keeps randomly choosing numbers until it gets a distinct number, so only use this function when n is relatively smaller than max - min.
+ */
 function randomIntegers(n: number, min: number, max: number) {
-    const numbers = [];
-    for (let _ = 0; _ < n; _++) {
+    const numbers: number[] = [];
+
+
+    for (let i = 0; i < n; i++) {
         let number = randomInteger(min, max);
-        while (numbers.findIndex(v => v === number) !== -1) {
+
+        while (numbers.findIndex(value => value === number) !== -1) {
             number = randomInteger(min, max);
         }
+
+
         numbers.push(number);
     }
+
+
     return numbers;
 }
+
+
+
 
 async function main() {
     await prisma.user.create({
@@ -67,8 +91,10 @@ async function main() {
         data: { id: 0 },
     });
 
+
     if (process.env.SEED_RANDOM_DATA === 'true') {
         const difficulties = [];
+
         for (let i = 0; i < 10; i++) {
             difficulties.push(
                 await prisma.difficulty.create({
@@ -82,7 +108,9 @@ async function main() {
             );
         }
 
+
         const techs = [];
+
         for (let i = 1; i <= 10; i++) {
             techs.push(
                 await prisma.tech.create({
@@ -94,7 +122,9 @@ async function main() {
             );
         }
 
+
         const qualities = [];
+
         for (let i = 1; i <= 10; i++) {
             qualities.push(
                 await prisma.quality.create({
@@ -107,7 +137,9 @@ async function main() {
             );
         }
 
+
         const lengths = [];
+
         for (let i = 1; i <= 10; i++) {
             lengths.push(
                 await prisma.length.create({
@@ -117,12 +149,16 @@ async function main() {
                         order: i,
                     }
                 })
-            )
+            );
         }
 
+
         const users = [];
+
         for (let i = 0; i < 200; i++) {
             const name = 'User' + randomString(30);
+
+
             users.push(
                 await prisma.user.create({
                     data: {
@@ -138,9 +174,10 @@ async function main() {
             );
         }
 
+
+        const publisherGameBananaIds = randomIntegers(20, 0, 10000000); // GameBananaIds must be unique.
         const publishers = [];
-        // GameBananaIds must be unique.
-        const publisherGameBananaIds = randomIntegers(20, 0, 10000000);
+
         for (let i = 0; i < publisherGameBananaIds.length; i++) {
             publishers.push(
                 await prisma.publisher.create({
@@ -152,14 +189,17 @@ async function main() {
             );
         }
 
+
         const modTypes = [ModType.Normal, ModType.Collab, ModType.Contest, ModType.LobbyOther];
+        const modGameBananaIds = randomIntegers(50, 1000, 300000);  // GameBananaIds must be unique.
         const mods = [];
-        // GameBananaIds must be unique.
-        const modGameBananaIds = randomIntegers(50, 1000, 300000);
+
         for (let i = 0; i < modGameBananaIds.length; i++) {
             const timeCreatedGamebanana = randomInteger(1500000000, 160000000);
             const timeSubmitted = randomInteger(timeCreatedGamebanana, timeCreatedGamebanana + 1000000);
             const timeApproved = randomInteger(timeSubmitted, timeSubmitted + 1000000);
+
+
             mods.push(
                 await prisma.mod.create({
                     data: {
@@ -178,11 +218,15 @@ async function main() {
             );
         }
 
+
         const maps = [];
+
         for (let i = 0; i < 30; i++) {
             const mod = randomElement(mods)!;
             const timeSubmitted = randomInteger(mod.timeApproved, mod.timeApproved + 10000000);
             const timeApproved = randomInteger(timeSubmitted, timeSubmitted + 1000000);
+
+
             maps.push(
                 await prisma.map.create({
                     data: {
@@ -195,16 +239,19 @@ async function main() {
                         lengthId: randomElement(lengths)!.id,
                     }
                 })
-            )
+            );
         }
 
+
         const ratings = [];
-        // The combination of map and submitted by must be unique.
-        const mapsAndSubmittedBy = randomIntegers(20, 0, maps.length * users.length);
+        const mapsAndSubmittedBy = randomIntegers(20, 0, maps.length * users.length);   //The combination of map and submitted by must be unique.
+
         for (let i = 0; i < mapsAndSubmittedBy.length; i++) {
             const mapAndSubmittedBy = mapsAndSubmittedBy[i]!;
             const map = maps[Math.floor(mapAndSubmittedBy / users.length)]!;
             const submittedBy = users[mapAndSubmittedBy % users.length]!.id;
+
+
             ratings.push(
                 await prisma.rating.create({
                     data: {
