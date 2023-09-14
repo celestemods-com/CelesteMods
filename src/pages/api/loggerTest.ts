81ad78c4e60c158b0bd704e95691ab4a8b8f1476
otobot1 <otobot1@gmail.com>
2023-09-14 16:13:17 -0600

    finalized logger config

--- src/pages/api/loggerTest.ts
+++ src/pages/api/loggerTest.ts
@@ -1,8 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { logger } from "logger/logger.cjs";
+import { logger } from "logger/logger";
 
 
 export default function (_req: NextApiRequest, res: NextApiResponse) {
+    logger.fatal("Logging fatal from loggerTest.ts");
+    logger.error("Logging error from loggerTest.ts");
     logger.warn("Logging warn from loggerTest.ts");
     logger.info("Logging info from loggerTest.ts");
     logger.trace("Logging trace from loggerTest.ts");
