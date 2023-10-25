4dba6b456aee2fdb76ce4eb4803d1fa87a349663
otobot1 <otobot1@gmail.com>
2023-10-25 13:01:51 -0600

    Extract Page Pathnames Into Consts

--- src/pages/comingSoon.tsx
+++ src/pages/comingSoon.tsx
@@ -1,9 +1,13 @@
 import { type NextPage } from "next";
 import { Layout } from "~/components/layout/layout";
+import { COMING_SOON_PATHNAME } from "~/consts/pathnames";
+
+
+
 
 const ComingSoon: NextPage = () => {
   return (
-    <Layout pageTitle="Coming Soon" pageDescription="Coming Soon" pathname="/comingSoon">
+    <Layout pageTitle="Coming Soon" pageDescription="Coming Soon" pathname={COMING_SOON_PATHNAME}>
       <h1>Coming soon</h1>
       <p>The site is currently in early beta. This feature has not been completed yet.</p>
     </Layout>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,10 @@ import { signIn, signOut, useSession } from "next-auth/react";
 
 import { api } from "~/utils/api";
 import { Layout } from "~/components/layout/layout";
+import { MODS_PAGE_PATHNAME } from "~/consts/pathnames";
+
+
+
 
 const Home: NextPage = () => {
 
@@ -12,7 +16,7 @@ const Home: NextPage = () => {
       <h1>Celeste Mods List</h1>
       <p>Welcome! The site is currently in early beta.</p>
       <Link
-        href={"/mods"}
+        href={MODS_PAGE_PATHNAME}
       >
         You can only browse mods for now.
       </Link>
