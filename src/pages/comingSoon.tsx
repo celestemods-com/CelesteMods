842f298df1cf03be0b934685d9ab75e96e916e8f
otobot1 <otobot1@gmail.com>
2023-11-15 16:36:43 -0700

    Update comingSoon.tsx
    Capitalization

--- src/pages/comingSoon.tsx
+++ src/pages/comingSoon.tsx
@@ -8,7 +8,7 @@ import { COMING_SOON_PATHNAME } from "~/consts/pathnames";
 const ComingSoon: NextPage = () => {
   return (
     <Layout pageTitle="Coming Soon" pageDescription="Coming Soon" pathname={COMING_SOON_PATHNAME}>
-      <h1>Coming soon</h1>
+      <h1>Coming Soon</h1>
       <p>The site is currently in early beta. This feature has not been completed yet.</p>
     </Layout>
   );
