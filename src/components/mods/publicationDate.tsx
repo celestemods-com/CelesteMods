782cdf00000cd0ab2f50f9419b5ab9312de23826
otobot1 <otobot1@gmail.com>
2024-02-26 17:52:08 -0700

    Fix PUBLICATION_DATE LABEL not displaying

--- src/components/mods/publicationDate.tsx
+++ src/components/mods/publicationDate.tsx
@@ -18,7 +18,7 @@ const PUBLICATION_DATE_LABEL = "Published: ";
 const PublicationDate = ({ publicationDate }: PublicationDateProps) => {
     return (
         <Text size="md">
-            {PUBLICATION_DATE_LABEL + publicationDate === undefined ? "Undefined" : publicationDate?.toLocaleDateString()}
+            {`${PUBLICATION_DATE_LABEL}${publicationDate === undefined ? "Undefined" : publicationDate?.toLocaleDateString()}`}
         </Text>
     );
 };
