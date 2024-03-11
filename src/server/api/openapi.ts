61bf0a4922f7698343693e7f8c0fb3b1b8a2dec4
otobot1 <otobot1@gmail.com>
2024-03-10 18:47:42 -0600

    Update openapi.ts
    handle NEXT_PUBLIC_BASE_PATH !== ""

--- src/server/api/openapi.ts
+++ src/server/api/openapi.ts
@@ -10,6 +10,6 @@ export const openApiDocument = generateOpenApiDocument(
     {
         title: 'Celeste Mods List API',
         version: '1.0.0',
-        baseUrl: '/api',
+        baseUrl: `${process.env.NEXT_PUBLIC_BASE_PATH ? process.env.NEXT_PUBLIC_BASE_PATH : ""}/api`,
     },
 );
