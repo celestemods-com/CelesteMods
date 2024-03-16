b63e88efdfec5fa22cf5667841c7b44217729113
otobot1 <otobot1@gmail.com>
2024-03-16 16:03:30 -0600

    Update primaryDisabled colors

--- src/styles/difficultyColors.ts
+++ src/styles/difficultyColors.ts
@@ -29,50 +29,50 @@ export type DifficultyColorsObjects = typeof difficultyColors[keyof typeof diffi
 export const difficultyColors = {
     beginner: {
         primary: "#16367b", // Adjusted Color - Contrast Ratios: Intermediate (1.47), Advanced (1.59), Expert (2.20), Grandmaster (1.23), Astral (), Celestial ()
-        primaryHover: "#84a4e9", // Adjusted Color - Contrast Ratio with Primary: 4.58 - TODO: make text color darker when using this color
-        primaryDisabled: "#0f172e",
-        secondary: "#4f942f",
-        secondaryHover: "#798fd3",
+        primaryHover: "#84a4e9", // Adjusted Color - Contrast Ratio with Primary: 4.58
+        primaryDisabled: "#5c72a3", // Adjusted Color - Contrast Ratio with Primary: 2.37 - WCAG guidelines don't require a minimum contrast ratio for disabled elements
+        secondary: "#4f942f",   // Adjusted Color - Contrast Ratios: Primary: 3.03, PrimaryHover: , PrimaryDisabled: , SecondaryHover:
+        secondaryHover: "#b8fd98",
     },
     intermediate: {
         primary: "#0f5881", // Adjusted Color - Contrast Ratios: Beginner (1.47), Advanced (1.07), Expert (1.49), Grandmaster (1.19), Astral (), Celestial ()
         primaryHover: "#87d0f9", // Adjusted Color - Contrast Ratio with Primary: 4.54
-        primaryDisabled: "#1f4869",
+        primaryDisabled: "#5c89a3", // Adjusted Color - Contrast Ratio with Primary: 2.03 - WCAG guidelines don't require a minimum contrast ratio for disabled elements
         secondary: "#3891d1",
         secondaryHover: "#66aadf",
     },
     advanced: {
         primary: "#595667", // Adjusted Color - Contrast Ratios: Beginner (1.59), Intermediate (1.07), Expert (1.37), Grandmaster (1.29), Astral (), Celestial ()
         primaryHover: "#d1cedf", // Adjusted Color - Contrast Ratio with Primary: 4.60
-        primaryDisabled: "#8a4d80",
+        primaryDisabled: "#242131", // Adjusted Color - Contrast Ratio with Primary: 2.20 - WCAG guidelines don't require a minimum contrast ratio for disabled elements
         secondary: "#b66fc5",
         secondaryHover: "#d494df",
     },
     expert: {
         primary: "#875aaa", // Adjusted Color - Contrast Ratios: Beginner (2.20), Intermediate (1.49), Advanced (1.37), Grandmaster (1.78), Astral (), Celestial ()
         primaryHover: "#f8f0ff", // Adjusted Color - Contrast Ratio with Primary: 4.64
-        primaryDisabled: "#833d70",
+        primaryDisabled: "#473753", // Adjusted Color - Contrast Ratio with Primary: 2.10 - WCAG guidelines don't require a minimum contrast ratio for disabled elements
         secondary: "#a336ad",
         secondaryHover: "#c362cd",
     },
     grandmaster: {
         primary: "#4c3795", // Adjusted Color - Contrast Ratios: Beginner (1.23), Intermediate (1.19), Advanced (1.29), Expert (1.78), Astral (), Celestial ()
         primaryHover: "#c1adff", // Adjusted Color - Contrast Ratio with Primary: 4.67
-        primaryDisabled: "#833d70",
+        primaryDisabled: "#161226", // Adjusted Color - Contrast Ratio with Primary: 1.99 - WCAG guidelines don't require a minimum contrast ratio for disabled elements
         secondary: "#a336ad",
         secondaryHover: "#c362cd",
     },
     astral: {
         primary: "#4c3795", // Placeholder Color - Contrast Ratios: Beginner (1.23), Intermediate (1.19), Advanced (1.29), Expert (1.78), Grandmaster (), Celestial ()
         primaryHover: "#c1adff", // Placeholder Color - Contrast Ratio with Primary: 
-        primaryDisabled: "#833d70",
+        primaryDisabled: "#161226",
         secondary: "#a336ad",
         secondaryHover: "#c362cd",
     },
     celestial: {
         primary: "#4c3795", // Placeholder Color - Contrast Ratios: Beginner (1.23), Intermediate (1.19), Advanced (1.29), Expert (1.78), Grandmaster (), Astral ()
         primaryHover: "#c1adff", // Placeholder Color - Contrast Ratio with Primary: 
-        primaryDisabled: "#833d70",
+        primaryDisabled: "#161226",
         secondary: "#a336ad",
         secondaryHover: "#c362cd",
     },
