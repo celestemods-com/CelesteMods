f60cb4bb802d25c2f6cd658501736b2fa03c41e4
otobot1 <otobot1@gmail.com>
2024-04-02 13:07:09 -0600

    update comment

--- src/styles/layoutColors.ts
+++ src/styles/layoutColors.ts
@@ -1,1 +1,1 @@
-export const blackBackgroundColor = "rgb(25, 25, 25)";    // changed from "rgba(1, 1, 1, 0.9)" because the opacity causes problem when applying this color to the difficulty tab container. this is the same color though.
+export const blackBackgroundColor = "rgb(25, 25, 25)";    // changed from "rgba(1, 1, 1, 0.9)" because the opacity causes problems when applying this color to the difficulty tab container. this is the same color though according to the table at the end of this article https://www.viget.com/articles/equating-color-and-transparency/#table
