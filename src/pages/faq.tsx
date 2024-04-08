93059f5a830f36a14375281d3955de60db794ff4
otobot1 <otobot1@gmail.com>
2024-04-07 23:44:52 -0600

    add underline to faq links

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -28,6 +28,9 @@ const useStyles = createStyles(
             scrollArea: {
                 height: `${pageContentHeightPixels}px`,
             },
+            link: {
+                textDecoration: "underline",
+            }
         });
     }
 );
@@ -57,20 +60,20 @@ const FAQ: NextPage = () => {
                     <h2>Installing Mods</h2>
                     <h3>Everest</h3>
                     <p>
-                        Everest is the modding API for Celeste. It is required to install most mods, and is installed automatically when setting up Olympus. If you need to install Everest manually, you can find it on their <Link href={"https://github.com/EverestAPI/Everest/releases"}>GitHub</Link>.
+                        Everest is the modding API for Celeste. It is required to install most mods, and is installed automatically when setting up Olympus. If you need to install Everest manually, you can find it on their <Link href={"https://github.com/EverestAPI/Everest/releases"} className={classes.link}>GitHub</Link>.
                     </p>
                     <h3 id="mod_managers">Mod Managers</h3>
                     <h4>Olympus</h4>
                     <p>
-                        Olympus is the recommended mod manager for Celeste. Everest is installed automatically during setup. You can find installation instructions for Windows, Mac, and Linux on the <Link href={OLYMPUS_INSTALLATION_URL}>Everest website</Link>.
+                        Olympus is the recommended mod manager for Celeste. Everest is installed automatically during setup. You can find installation instructions for Windows, Mac, and Linux on the <Link href={OLYMPUS_INSTALLATION_URL} className={classes.link}>Everest website</Link>.
                     </p>
                     <h4>CeleMod</h4>
                     <p>
-                        CeleMod is a new alternative mod manager for Celeste. You can find installation instructions on <Link href={"https://gamebanana.com/tools/16200"}>GameBanana</Link>.
+                        CeleMod is a new alternative mod manager for Celeste. You can find installation instructions on <Link href={"https://gamebanana.com/tools/16200"} className={classes.link}>GameBanana</Link>.
                     </p>
                     <h4>Mons</h4>
                     <p>
-                        Mons is a commandline Everest installer and mod manager for Celeste. You can find installation instructions on <Link href={"https://github.com/coloursofnoise/mons"}>their GitHub</Link>.
+                        Mons is a commandline Everest installer and mod manager for Celeste. You can find installation instructions on <Link href={"https://github.com/coloursofnoise/mons"} className={classes.link}>their GitHub</Link>.
                     </p>
                     <h2>Other FAQ</h2>
                     <p>Coming soon!</p>
