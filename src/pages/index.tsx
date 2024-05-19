5a03c8295c5688396b59bb95ac5f1419ba525a19
otobot1 <otobot1@gmail.com>
2024-05-18 23:02:39 -0600

    tweak text and add embed image from lego site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
 import { createStyles } from "@mantine/core";
 import { type NextPage } from "next";
+import Image from "next/image";
 import Link from "next/link";
 import { Layout } from "~/components/layout/layout";
 import { MODS_PAGE_PATHNAME } from "~/consts/pathnames";
+
+
+
+
+const LEGO_CELESTE_IMAGE_URL = "https://ideascdn.lego.com/media/generate/lego_ci/b31418aa-0482-4a00-b66a-258807f3ea96/resize:950:633/webp";
+
 
 
 
@@ -14,8 +21,13 @@ const useStyles = createStyles(
   }),
 );
 
+
+
+
 const Home: NextPage = () => {
   const { classes } = useStyles();
+  const height = 280;
+  const width = height / 577 * 867;
 
   return (
     <Layout
@@ -31,11 +43,17 @@ const Home: NextPage = () => {
       >
         You can only browse mods for now.
       </Link>
-      <h2>Community projects</h2>
-      <h3>Celeste Mountain Lego</h3>
+      <h2>Community Projects</h2>
+      <h3 style={{marginTop: "2px"}}>Celeste Mountain Lego Idea</h3>
+      <Image
+        src={LEGO_CELESTE_IMAGE_URL}
+        alt="A render of the proposed Celeste Mountain Lego set."
+        height={height}
+        width={width}
+      />
       <p>
         A lego set for Celeste. 
-        You can check it out <a href="https://ideas.lego.com/projects/ac89cbf0-9d0b-401d-b8cb-5316056d498f" className={classes.link}>here</a>.
+        If you want this to exist, vote for it <a href="https://ideas.lego.com/projects/ac89cbf0-9d0b-401d-b8cb-5316056d498f" className={classes.link}>here</a>.
       </p>
     </Layout>
   );
