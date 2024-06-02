f75912e349db08cca28235c80bd0b12b836244f0
otobot1 <otobot1@gmail.com>
2024-06-02 15:22:35 -0600

    add "Known Issues" section to FAQ
    - split FAQ page into 2 columns
    - add #724 to known issues

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -12,7 +12,9 @@ import { pageTitle } from "~/styles/pageTitle";
 const useStyles = createStyles(
     (theme) => {
         return ({
-            flexContainer: {
+            scrollArea: {
+                height: `${pageContentHeightPixels}px`,
+                color: theme.white,
                 "h2": {
                     margin: `0 ${theme.spacing.sm} ${theme.spacing.xs}`,
                 },
@@ -26,9 +28,8 @@ const useStyles = createStyles(
                     margin: `0 ${theme.spacing.sm} ${theme.spacing.xs}`,
                 },
             },
-            scrollArea: {
-                height: `${pageContentHeightPixels}px`,
-                color: theme.white,
+            innerFlex: {
+                flex: 1,
             },
             link: {
                 textDecoration: "underline",
@@ -59,30 +60,47 @@ const FAQ: NextPage = () => {
                 className={classes.scrollArea}
             >
                 <Flex
-                    direction={"column"}
-                    wrap={"wrap"}
-                    className={classes.flexContainer}
+                    direction="row"
+                    wrap="nowrap"
+                    justify="space-between"
                 >
-                    <h2>Installing Mods</h2>
-                    <h3>Everest</h3>
-                    <p>
-                        Everest is the modding API for Celeste. It is required to install most mods, and is installed automatically when setting up Olympus. If you need to install Everest manually, you can find it on their <Link href={"https://github.com/EverestAPI/Everest/releases"} className={classes.link}>GitHub</Link>.
-                    </p>
-                    <h3 id="mod_managers">Mod Managers</h3>
-                    <h4>Olympus</h4>
-                    <p>
-                        Olympus is the recommended mod manager for Celeste. Everest is installed automatically during setup. You can find installation instructions for Windows, Mac, and Linux on the <Link href={OLYMPUS_INSTALLATION_URL} className={classes.link}>Everest website</Link>.
-                    </p>
-                    <h4>CeleMod</h4>
-                    <p>
-                        CeleMod is a new alternative mod manager for Celeste. You can find installation instructions on <Link href={"https://gamebanana.com/tools/16200"} className={classes.link}>GameBanana</Link>.
-                    </p>
-                    <h4>Mons</h4>
-                    <p>
-                        Mons is a commandline Everest installer and mod manager for Celeste. You can find installation instructions on <Link href={"https://github.com/coloursofnoise/mons"} className={classes.link}>their GitHub</Link>.
-                    </p>
-                    <h2>Other FAQ</h2>
-                    <p>Coming soon!</p>
+                    <Flex
+                        className={classes.innerFlex}
+                        direction="column"
+                        wrap="wrap"
+                    >
+                        <h2>Installing Mods</h2>
+                        <h3>Everest</h3>
+                        <p>
+                            Everest is the modding API for Celeste. It is required to install most mods, and is installed automatically when setting up Olympus. If you need to install Everest manually, you can find it on their <Link href={"https://github.com/EverestAPI/Everest/releases"} className={classes.link}>GitHub</Link>.
+                        </p>
+                        <h3 id="mod_managers">Mod Managers</h3>
+                        <h4>Olympus</h4>
+                        <p>
+                            Olympus is the recommended mod manager for Celeste. Everest is installed automatically during setup. You can find installation instructions for Windows, Mac, and Linux on the <Link href={OLYMPUS_INSTALLATION_URL} className={classes.link}>Everest website</Link>.
+                        </p>
+                        <h4>CeleMod</h4>
+                        <p>
+                            CeleMod is a new alternative mod manager for Celeste. You can find installation instructions on <Link href={"https://gamebanana.com/tools/16200"} className={classes.link}>GameBanana</Link>.
+                        </p>
+                    </Flex>
+                    <Flex
+                        className={classes.innerFlex}
+                        direction="column"
+                        wrap="wrap"
+                    >
+                        <h4>Mons</h4>
+                        <p>
+                            Mons is a commandline Everest installer and mod manager for Celeste. You can find installation instructions on <Link href={"https://github.com/coloursofnoise/mons"} className={classes.link}>their GitHub</Link>.
+                        </p>
+                        <h2>Known Issues</h2>
+                        <h4><Link href="https://github.com/celestemods-com/CelesteMods/issues/724">Difficulty Header Separated From Info Header</Link></h4>
+                        <p>
+                            The difficulty tabs are separated from the Mods Table in Safari and other Webkit-based browsers. We are aware of this issue and plan to fix it in the future, but it is not currently a high-priority.
+                        </p>
+                        <h2>Other FAQ</h2>
+                        <p>Coming soon!</p>
+                    </Flex>
                 </Flex>
             </ScrollArea>
         </Layout>
