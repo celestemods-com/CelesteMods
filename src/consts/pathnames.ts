0332b0ad04e842304d4f9d7e26a6ed1526e4ae63
otobot1 <otobot1@gmail.com>
2024-06-04 13:47:06 -0600

    add robots.txt

--- src/consts/pathnames.ts
+++ src/consts/pathnames.ts
@@ -1,3 +1,3 @@
 export const MODS_PAGE_PATHNAME = "/mods";
 export const FAQ_PAGE_PATHNAME = "/faq";
-export const COMING_SOON_PATHNAME = "/coming-soon";
+export const COMING_SOON_PATHNAME = "/coming-soon"; // must match the path in /pages/robots.txt
