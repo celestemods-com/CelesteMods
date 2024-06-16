8ff67cbcdae8f07e2c22286efc72a33b79c9a75d
otobot1 <otobot1@gmail.com>
2024-06-16 00:24:13 -0600

    remove NEXT_PUBLIC_BASE_PATH
    convert ' to "

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,6 @@ const withBundleAnalyzer = bundleAnalyzer({
 
 /** @type {import("next").NextConfig} */
 const config = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   reactStrictMode: true,
   images: {    //TODO!: figure out how to fix crash on next.config.mjs change
     remotePatterns: [
