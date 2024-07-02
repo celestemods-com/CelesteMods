190f7e5c9bac8366c3352723f2e9ccc6a3e48bab
otobot1 <otobot1@gmail.com>
2024-07-01 19:51:09 -0600

    refactor update-webhook.ts and genericize YAML logic

--- src/pages/api/gamebanana-mirror/update-webhook.ts
+++ src/pages/api/gamebanana-mirror/update-webhook.ts
@@ -1,21 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { writeFile, readFile } from "fs/promises";
-import { parse } from "yaml";
 import { serverLogger as logger } from "~/logger/serverLogger";
 import { sendSuccessSignal } from "~/server/gamebananaMirror/sendSuccessSignal";
 import { authenticateUpdateWebhookRequest } from "~/server/gamebananaMirror/authentication/authenticateUpdateWebhookRequest";
+import { type ModSearchDatabase, modSearchDatabaseFileSystemErrorString, getUpdatedModSearchDatabase } from "~/server/gamebananaMirror/yamlHandlers/modSearchDatabase";
+import { updateGamebananaMirror } from "~/server/gamebananaMirror/updateGamebananaMirror";
 
 
-
-
-const MOD_SEARCH_DATABASE_YAML_URL = "https://maddie480.ovh/celeste/mod_search_database.yaml";
-
-const MOD_SEARCH_DATABASE_JSON_PATH = process.env.MOD_SEARCH_DATABASE_PATH || "./public/mod_search_database.json";
-
-const MOD_SEARCH_DATABASE_FILE_ENCODING = "utf-8";
-
-
-const FILE_SYSTEM_ERROR_STRING = "Failed to write the Mod Search Database to the file system.";
 
 
 const GAMEBANANA_MOD_DOWNLOAD_BASE_URL = "https://gamebanana.com/dl/";
@@ -25,74 +15,10 @@ const FILE_CATEGORIES = ["mods", "screenshots", "richPresenceIcons"] as const sa
 
 
 
-export type ModSearchDatabase = Record<string, unknown>;
-
-
-
-
-const isValidModSearchDatabase = (value: unknown): value is ModSearchDatabase => {
-    return true;    //TODO!!!: Implement this
-};
-
-
-
-
-/** Updates the mod search database json file.
- * Also returns the parsed and validated object.
-*/
-const getNewModSearchDatabase = async (): Promise<ModSearchDatabase> => {
-    logger.trace("Downloading the Mod Search Database.");
-
-
-    const response = await fetch(MOD_SEARCH_DATABASE_YAML_URL);
-
-    if (!response.ok) {
-        throw `Failed to download the Mod Search Database. Status code: ${response.status}`;
-    }
-
-    logger.trace("Successfully downloaded the Mod Search Database.");
-
-
-    const newYaml = await response.text();
-
-    const parsedYaml: unknown = parse(newYaml);
-
-    if (!isValidModSearchDatabase(parsedYaml)) {
-        throw "The downloaded Mod Search Database failed validation.";
-    }
-
-    logger.trace("Successfully parsed the Mod Search Database.");
-
-
-    try {
-        await writeFile(MOD_SEARCH_DATABASE_JSON_PATH, JSON.stringify(parsedYaml), MOD_SEARCH_DATABASE_FILE_ENCODING);
-    } catch (error) {
-        logger.error(`${FILE_SYSTEM_ERROR_STRING} ${error}`);
-
-        throw FILE_SYSTEM_ERROR_STRING;
-    }
-
-
-    return parsedYaml;
-};
-
-
-
-
-/** Updates the GameBanana mirror.
- * Returns the HTTP status code of the update.
-*/
-const updateGamebananaMirror = async (modSearchDatabase: ModSearchDatabase): Promise<number> => {
-    //TODO!!!: Implement this
-    //continue here
-};
-
-
-
-
 /** Downloads the new Mod Search Database before sending a response.
  * Sends a 200 status code if the download was successful.
  * Sends a 500 status code if the download was unsuccessful.
+ * Sends 40X or 500 status codes if the authentication fails.
 */
 const handler = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
     logger.trace("GameBanana mirror update request received.");
@@ -113,9 +39,9 @@ const handler = async (req: NextApiRequest, res: NextApiResponse): Promise<void>
     let modSearchDatabase: ModSearchDatabase;
 
     try {
-        modSearchDatabase = await getNewModSearchDatabase();
+        modSearchDatabase = await getUpdatedModSearchDatabase();
     } catch (error) {
-        if (error !== FILE_SYSTEM_ERROR_STRING) {
+        if (error !== modSearchDatabaseFileSystemErrorString) {
             logger.error(error);
         }
 
---
+++ src/server/gamebananaMirror/updateGamebananaMirror.ts
@@ -0,0 +1,13 @@
+import { type ModSearchDatabase } from "./yamlHandlers/modSearchDatabase";
+
+
+
+
+
+/** Updates the GameBanana mirror.
+ * Returns the HTTP status code of the update.
+*/
+export const updateGamebananaMirror = async (modSearchDatabase: ModSearchDatabase): Promise<number> => {
+    //TODO!!!: Implement this
+    //continue here
+};
