09f6925e77a1c5352b84c7b99fcdb41e2b999ffa
otobot1 <otobot1@gmail.com>
2024-08-17 14:28:29 -0600

    fix mod search database not validating when calling getCurrentModSearchDatabase
    add console logs

--- src/server/gamebananaMirror/yamlHandlers/modSearchDatabase.ts
+++ src/server/gamebananaMirror/yamlHandlers/modSearchDatabase.ts
@@ -42,11 +42,17 @@ export type ModSearchDatabase = ModSearchDatabase_ModInfo[];
 
 
 const isValidModSearchDatabase_ModInfo_Screenshots = (value: unknown): value is ModSearchDatabase_ModInfo["Screenshots"] => {
-    if (!Array.isArray(value) || value.length === 0) return false;
+    if (!Array.isArray(value)) {
+        console.log("value is not an array");
+        return false;
+    }
 
 
     for (const screenshot of value) {
-        if (typeof screenshot !== "string" || !screenshot.startsWith(GAMEBANANA_MOD_IMAGES_BASE_URL)) return false;
+        if (typeof screenshot !== "string" || !screenshot.startsWith(GAMEBANANA_MOD_IMAGES_BASE_URL)) {
+            console.log("screenshot is not a string or does not start with the base url");
+            return false;
+        }
     }
 
 
@@ -55,21 +61,36 @@ const isValidModSearchDatabase_ModInfo_Screenshots = (value: unknown): value is 
 
 
 const isValidModSearchDatabase_ModInfo_Files = (value: unknown): value is ModSearchDatabase_ModInfo["Files"] => {
-    if (!Array.isArray(value) || value.length === 0) return false;
+    if (!Array.isArray(value)) {
+        console.log("value is not an array");
+        return false;
+    }
 
 
     for (const file of value) {
-        if (typeof file !== "object" || file === null || "URL" in file === false || "CreatedDate" in file === false) return false;
+        if (typeof file !== "object" || file === null || "URL" in file === false || "CreatedDate" in file === false) {
+            console.log("file is not an object or is null");
+            return false;
+        }
 
-        if (typeof file.URL !== "string" || !file.URL.startsWith(MOD_SEARCH_DATABASE_FILE_URL_PREFIX)) return false;
+        if (typeof file.URL !== "string" || !file.URL.startsWith(MOD_SEARCH_DATABASE_FILE_URL_PREFIX)) {
+            console.log("URL is not a string or does not start with the base url");
+            return false;
+        }
 
 
         const fileId = Number(file.URL.slice(MOD_SEARCH_DATABASE_FILE_URL_PREFIX.length));
 
-        if (isNaN(fileId) || fileId <= 0) return false;
+        if (isNaN(fileId) || fileId <= 0) {
+            console.log("fileId is not a number or is less than or equal to 0");
+            return false;
+        }
 
 
-        if (typeof file.CreatedDate !== "number" || file.CreatedDate <= 0) return false;
+        if (typeof file.CreatedDate !== "number" || file.CreatedDate <= 0) {
+            console.log("CreatedDate is not a number or is less than or equal to 0");
+            return false;
+        }
     }
 
 
@@ -80,6 +101,7 @@ const isValidModSearchDatabase_ModInfo_Files = (value: unknown): value is ModSea
 /** Only validates the parts of the object that this repository consumes. */
 const isValidModSearchDatabase = (value: unknown): value is ModSearchDatabase => {
     if (!Array.isArray(value) || value.length === 0) {
+        console.log("value is not an array or is empty");
         return false;
     }
 
@@ -87,13 +109,26 @@ const isValidModSearchDatabase = (value: unknown): value is ModSearchDatabase =>
     const modSearchDatabase = value as ModSearchDatabase;
 
     for (const mod of modSearchDatabase) {
-        if (typeof mod !== "object" || mod === null || "GameBananaId" in mod === false || "Screenshots" in mod === false || "Files" in mod === false) return false;
+        if (typeof mod !== "object" || mod === null || "GameBananaId" in mod === false || "Screenshots" in mod === false || "Files" in mod === false) {
+            console.log("mod is not an object or is null");
+            return false;
+        }
 
-        if (typeof mod.GameBananaId !== "number") return false;
+        if (typeof mod.GameBananaId !== "number") {
+            console.log("GameBananaId is not a number");
+            return false;
+        }
 
-        if (!isValidModSearchDatabase_ModInfo_Screenshots(mod.Screenshots)) return false;
+        if (!isValidModSearchDatabase_ModInfo_Screenshots(mod.Screenshots)) {
+            console.log("Screenshots are invalid");
+            return false;
+        }
 
-        if (!isValidModSearchDatabase_ModInfo_Files(mod.Files)) return false;
+        if (!isValidModSearchDatabase_ModInfo_Files(mod.Files)) {
+            console.log("Files are invalid");
+            console.log(JSON.stringify(mod))
+            return false;
+        }
     }
 
 
