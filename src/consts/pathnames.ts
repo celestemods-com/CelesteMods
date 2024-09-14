143071470e2302f7dbc015a4bfba1de023805c2c
otobot1 <otobot1@gmail.com>
2024-09-13 23:08:30 -0600

    tweak page URLs
    combine user-claim related pages into single folder

--- src/consts/pathnames.ts
+++ src/consts/pathnames.ts
@@ -2,4 +2,4 @@ export const MODS_PAGE_PATHNAME = "/mods";
 export const FAQ_PAGE_PATHNAME = "/faq";
 export const COMING_SOON_PATHNAME = "/coming-soon";
 export const CLAIM_USER_PATHNAME = "/claim-user";
-export const VERIFY_CLAIM_PATHNAME = "/verify-claim";
+export const VERIFY_CLAIM_PATHNAME = "/claim-user/verify";
